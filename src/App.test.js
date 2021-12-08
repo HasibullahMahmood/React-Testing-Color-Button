2371df67ce87eed4da16ecf49cb337bc0cb9820a
Hasibullah Mahmood 2 <hasibullahmahmod@gmail.com>
2021-12-08 15:57:27 +0300

    initail test implemented

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Change bg color button work properly', () => {
+	render(<App />);
+	const button = screen.getByRole('button', { name: 'Change to blue' });
+	expect(button).toHaveStyle({ backgroundColor: 'red' });
+	fireEvent.click(button);
+	expect(button).toHaveStyle({ backgroundColor: 'blue' });
+	expect(button.textContent).toBe('Change to red');
 });
